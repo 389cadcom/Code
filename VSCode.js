f3a7299cabab8debabc7757d02305b6a396e933e
389cadcom <389cadcom@163.com>
2017-05-18 18:29:52 +0800

    vscode

---
+++ VSCode.js
@@ -0,0 +1,58 @@
+//gist: 710240a39040bcea40ede9509d7169c152d29972
+
+1.带snippets 一般是代码提示类扩展；
+2.带viewer 一般是代码运行预览类扩展；
+3.带support 一般是代码语言支持；
+4.带document 一般是参考文档类扩展；
+5.带Formatt 一般是代码格式化整理扩展；
+
+tab
+1. ! 生成html
+
+http://blog.csdn.net/u011127019/article/details/52263347
+
+
+2.Ctrl+Alt+鼠标左键  选中多项
+
+3.将自动高亮的变量、字符一次性替换：
+双击变量，右键‘更改所有匹配项’。
+
+双击变量，Ctrl+F2
+
+4.跳转到定义处：F12
+
+
+5.如果要重置同步设置：按ctrl+p输入'>sync'
+
+6.项目切换  Ctrl+P  >proj edit
+
+
+/**VSCode整理*/
+
+auto-close-tag - Version :0.4.1								//自动关闭标签
+auto-rename-tag - Version :0.0.11							//自动重命名标签
+beautify - Version :1.0.2									//格式化代码的工具
+bootstrap-3-snippets - Version :0.0.9						//bootstrap3代码生成工具
+code-settings-sync - Version :2.7.0							//设置同步
+debugger-for-chrome - Version :3.1.1
+docthis - Version :0.4.4
+html-snippets - Version :0.1.0
+JavaScriptSnippets - Version :1.4.0
+jquerysnippets - Version :0.0.1
+npm-intellisense - Version :1.3.0
+open-in-browser - Version :1.0.0
+output-colorizer - Version :0.1.1
+path-intellisense - Version :1.4.2
+project-manager - Version :0.16.0
+theme-atom-one-dark - Version :0.0.1
+vetur - Version :0.6.10										//Vue高亮
+vscode-caniuse - Version :0.5.0
+vscode-csscomb - Version :4.0.1
+vscode-eclipse-keybindings - Version :0.9.3
+vscode-gulp - Version :0.0.4
+vscode-html-css - Version :0.1.4
+vscode-icons - Version :7.7.0								//文件图标
+vscode-javascript-snippet-pack - Version :0.1.5
+vscode-JS-CSS-HTML-formatter - Version :0.2.3				//格式化
+vscode-scss - Version :0.6.2
+vscode-todo-parser - Version :1.8.10
