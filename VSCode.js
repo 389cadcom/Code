fd2c9397da3c544b9e02483a8f6a785c2ffbe846
389cadcom <389cadcom@163.com>
2017-08-11 18:06:48 +0800

    Settings Sync

--- VSCode.js
+++ VSCode.js
@@ -1,14 +1,15 @@
-//gist: 710240a39040bcea40ede9509d7169c152d29972
+//gist: 6fde01f8bfe6f6c84b52dfa1793e1414524d6460
 
-1.带snippets 一般是代码提示类扩展；
-2.带viewer 一般是代码运行预览类扩展；
-3.带support 一般是代码语言支持；
-4.带document 一般是参考文档类扩展；
-5.带Formatt 一般是代码格式化整理扩展；
+1.带snippets	一般是代码提示类扩展；
+2.带viewer		一般是代码运行预览类扩展；
+3.带support		一般是代码语言支持；
+4.带document	一般是参考文档类扩展；
+5.带Formatt		一般是代码格式化整理扩展；
 
 tab
 1. ! 生成html
 
+Ement
 http://blog.csdn.net/u011127019/article/details/52263347
 
 
@@ -22,7 +23,9 @@ http://blog.csdn.net/u011127019/article/details/52263347
 4.跳转到定义处：F12
 
 
-5.如果要重置同步设置：按ctrl+p输入'>sync'
+5.如果要重置同步设置：按ctrl+p输入'>sync'    //ctrl+shift+p
+upload: shift+alt+u, downland: shift+alt+d
+
 
 6.项目切换  Ctrl+P  >proj edit
 
