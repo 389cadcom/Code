8592b1b8b580a25f623be69bbc5f301d92621f2e
389cadcom <389cadcom@163.com>
2017-09-26 20:38:22 +0800

    babel

--- Babel/babel插件.js
+++ Babel/babel插件.js
@@ -1,12 +1,12 @@
 //解析，转换和生成
 {   presets: [],   plugins: [] }
 
+//{presets: ['es2015']}
+
 //预设presets  --> plugins的组合				http://babeljs.io/docs/plugins/
 env, es2015, es2016, es2017, latest, react		//latest  --> babel-preset-env代替
 
-
-//{paresets: ['es2015']}
-
+//es2015
 check-es2015-constants					// 检验const常量是否被重新赋值
 transform-es2015-arrow-functions		// 编译箭头函数
 transform-es2015-block-scoped-functions	// 函数声明在作用域内
@@ -29,49 +29,66 @@ transform-es2015-typeof-symbol			// 编译Symbol类型
 transform-es2015-unicode-regex			// 正则添加unicode模式
 transform-regenerator					// 编译generator函数
 
+//es2016	--es7
+transform-exponentiation-operator		// 编译幂运算符
+
+//es2017	--es8
+syntax-trailing-function-commas			// function最后一个参数允许使用逗号
+transform-async-to-generator			// 把async函数转化成generator函数
 
 
-//Stage-X
-babel-preset-stage-4:					// 何转换都是尚未被批准为发布Javascript的语言
+/*
+	Stage-X(0/1/2/3/4) 征求、展求、草案、定稿
+
+	何转换都是尚未被批准为发布Javascript的语言
+*/
+
+//stage-4:
 syntax-trailing-function-commas			// function最后一个参数允许使用逗号（ES8已经存在）
 transform-async-to-generator			// 把async函数转化成generator函数（ES8已经存在）
 transform-exponentiation-operator		// 编译幂运算符（ES7已经存在）
 
-babel-preset-stage-3：					//除了stage-4的内容，还包括以下插件：
+babel-preset-stage-3：					// 除了stage-4的内容，还包括以下插件：
 transform-object-rest-spread			// 编译对象的解构赋值和不定参数
 transform-async-generator-functions		// 将async generator function和for await编译为es2015的generator。
 
 
-babel-preset-stage-2：					//除了stage-3的内容，还包括以下插件：
-
+//stage-2：								//除了stage-3的内容，还包括以下插件：
 syntax-dynamic-import					// 动态加载模块
 transform-class-properties				// 编译静态属性(es2015)和属性初始化语法声明的属性(es2016)。
 //transform-decorators					已禁用的等待提案更新（可以在此期间使用旧版转换）
 
 
-babel-preset-stage-1：
+//stage-1：
 transform-class-constructor-call(弃用)	// 编译class中的constructor，在Babel7中会被移除
-transform-export-extensions				// 编译额外的export语法，如export * as ns from "mod";细节可以看这个。
+transform-export-extensions				// 编译额外的export语法，如export * as ns from "mod";
 
 
-babel-preset-stage-0：
+//stage-0：
 transform-do-expressions				// 编译do表达式
 transform-function-bind					// 编译bind运算符，即::
 
 
 
-//配置参数
 {
-  "presets": ["env", options]
+	"presets": [ ["env", { "modules":false }], "stage-2"]
 }
+
+
+
+//plugins插件  默认只转换新语句，不转换API
+
+//Iterator、Generator、Set、Maps、Proxy、Reflect、Symbol、Promise等全局对象
+//全局对象上的方法Object.assign, Array.from
+
+
+babel-polyfill			//为当前环境提供一个垫片
+
+babel-runtime			//提供工具函数，减少重复代码	_extend()， classCallCheck()工具函数
 /*
-targets.node			支持到哪个版本的 node
-targets.browsers		支持到哪个版本的浏览器
-loose					启动宽松模式，配合 webpack 的 loader 使用
-modules					使用何种模块加载机制
-debug					开启调试模式
-include					包含哪些文件
-exclude					排除哪些文件
-useBuiltIns				是否对 babel-polyfill 进行分解，只引入所需的部分
+注：
+启用babel-plugin-transfrom-runtime，Babel就会使用babel-runtime工具函数，还会自动引用babel-polyfill
 */
 
+babel-register			//改写require命令
+//用 import 代替require，import的优点在于可以引入所需方法或者变量，而不需要加载整个模块，提高了性能
--- VSCode/sync.js
+++ VSCode/sync.js
@@ -22,6 +22,9 @@ vsicons.settings.json
 
 EXTENSIONS ADDED :
 
+
+Lodash Lodash									//代码段
+
 auto-close-tag - Version :0.5.0
 auto-rename-tag - Version :0.0.14
 bootstrap-3-snippets - Version :0.1.0
@@ -31,12 +34,12 @@ GBKtoUTF8 - Version :0.0.2
 html-snippets - Version :0.1.0
 JavaScriptSnippets - Version :1.4.1
 jquerysnippets - Version :0.0.1
-npm-intellisense - Version :1.3.0					//自动完成导入语句中的 npm 模块
+npm-intellisense - Version :1.3.0					//导入模块时，提示已安装模块名称
 open-in-browser - Version :1.1.0
 output-colorizer - Version :0.1.2
-path-autocomplete - Version :1.6.1
+path-autocomplete - Version :1.6.1					//路径完成提示
 path-intellisense - Version :1.4.2					//自动补全文件名
-project-manager - Version :0.21.1
+project-manager - Version :0.21.1					//快速切换项目
 theme-atom-one-dark - Version :0.0.1
 vetur - Version :0.9.8
 vscode-caniuse - Version :0.5.0
@@ -45,10 +48,12 @@ vscode-fileheader - Version :0.0.2
 vscode-gulp - Version :0.0.4
 vscode-html-css - Version :0.1.8
 vscode-html-scss - Version :0.0.42
-vscode-icons - Version :7.13.0
+vscode-icons - Version :7.13.0						//文件图标，方便定位文件
 vscode-javascript-snippet-pack - Version :0.1.5
 vscode-JS-CSS-HTML-formatter - Version :0.2.3
 vscode-scss - Version :0.6.2
-vscode-todo-parser - Version :1.9.1
-vuehelper - Version :0.1.0
+vscode-todo-parser - Version :1.9.1					
+vetur - Version :0.1.0								//Vue语法高亮
+vuehelper - Version :0.1.0							//Vue2代码段（包括Vue2 api、vue-router2、vuex2）
+vscode-vue-peek										//定位vue路径
 webpack - Version :2.0.2
