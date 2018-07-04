3968fef14c017d74d97a17c536d342238da80b36
389cadcom <389cadcom@163.com>
2018-07-04 22:52:59 +0800

    vue

---
+++ Vue/说明.js
@@ -0,0 +1,32 @@
+Vue.mixin, Vue.filter, Vue.directive, Vue.exend, Vue.component, Vue.compile
+
+Vue.use, Vue.nextTick, Vue.set, Vue.delete, 
+
+
+new Vue({
+	el: '#app',
+	props: [],
+	mixins: [],
+	data(){
+		return {
+			id: id,
+			page: 1,
+		}
+	},
+	watch: {
+		'$route'(to, from){
+		
+		},
+		// 如果路由有变化，会再次执行该方法
+    '$route': "fetchDate",
+		requestParams: {
+			handler: 'getData',
+			immediate: true
+		}
+	},
+	methods: {
+		getData(param = { id: this.id, page: this.page }){
+			
+		}
+	}
+})
