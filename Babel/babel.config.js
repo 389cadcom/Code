e0003c450dae2292fd5a5bd47a51043294f7a542
389cadcom <389cadcom@163.com>
2018-09-04 20:31:48 +0800

    babel7

---
+++ Babel/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function(){
+	const presets = [
+		
+	]
+	const plugins = [
+		
+	];
+	return {
+		presets,
+		plugins
+	}
+}
---
+++ Babel/babel7.js
@@ -0,0 +1,11 @@
+@babel/core @babel/cli 
+@babel/preset-env					 //@babel/plugin-transform-arrow-functions
+@babel/polyfill
+
+@babel/preset-react
+@babel/preset-typescript
+
+
+
+//Брвы
+babel src --out-dir lib --presets=@babel/env
